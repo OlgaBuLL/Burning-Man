dc6d7f8e17f10591e11da4ae507777df9a955c29
OlgaBuLL <108359930+OlgaBuLL@users.noreply.github.com>
2023-01-22 22:32:13 +0100

    grid about-event

--- assets/scripts/burning-man.js
+++ assets/scripts/burning-man.js
@@ -1,32 +1,4 @@
-// const btnUp = {
-//   el: document.querySelector(".btn-up"),
-//   show() {
-//     this.el.classList.remove("btn-up_hide"); // удалим у кнопки класс btn-up_hide
-//   },
-//   hide() {
-//     this.el.classList.add("btn-up_hide"); // добавим к кнопке класс btn-up_hide
-//   },
-//   addEventListener() {
-//     // при прокрутке содержимого страницы
-//     window.addEventListener("scroll", () => {
-//       // определяем величину прокрутки
-//       const scrollY = window.scrollY || document.documentElement.scrollTop;
-//       // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
-//       scrollY > 400 ? this.show() : this.hide();
-//     });
-//     // при нажатии на кнопку .btn-up
-//     document.querySelector(".btn-up").onclick = () => {
-//       // переместим в начало страницы
-//       window.scrollTo({
-//         top: 0,
-//         left: 0,
-//         behavior: "smooth",
-//       });
-//     };
-//   },
-// };
-
-// btnUp.addEventListener();
+// --------------- BUTTON UP ---------------//
 
 const btnUp = {
   el: document.querySelector(".btn-up"),
@@ -86,47 +58,7 @@ const btnUp = {
 
 btnUp.addEventListener();
 
-// // ---------МЕНЮ БУРГЕР ДЛЯ МОБИЛЬНОЙ ВЕРСИИ--------
-
-// const hamb = document.querySelector("#hamb");
-// const popup = document.querySelector("#popup");
-// const body = document.body;
-
-// // Клонируем меню, чтобы задать свои стили для мобильной версии
-// const menu = document.querySelector("#menu").cloneNode(1);
-
-// // При клике на иконку hamb вызываем функцию hambHandler
-// hamb.addEventListener("click", hambHandler);
-
-// // Выполняем действия при клике ..
-// function hambHandler(e) {
-//   e.preventDefault();
-//   // Переключаем стили элементов при клике
-//   popup.classList.toggle("open");
-//   hamb.classList.toggle("active");
-//   body.classList.toggle("noscroll");
-//   renderPopup();
-// }
-
-// // Здесь мы рендерим элементы в наш попап
-// function renderPopup() {
-//   popup.appendChild(menu);
-// }
-
-// // Код для закрытия меню при нажатии на ссылку
-// const links = Array.from(menu.children);
-
-// // Для каждого элемента меню при клике вызываем ф-ию
-// links.forEach((link) => {
-//   link.addEventListener("click", closeOnClick);
-// });
-
-// // Закрытие попапа при клике на меню
-// function closeOnClick() {
-//   popup.classList.remove("open");
-//   hamb.classList.remove("active");
-//   body.classList.remove("noscroll");
-// }
+// ---------МЕНЮ БУРГЕР ДЛЯ МОБИЛЬНОЙ ВЕРСИИ--------
 
 let headerBurger = document.querySelector(".header__burger");
 let headerList = document.querySelector(".header__list");
@@ -136,3 +68,11 @@ headerBurger.addEventListener("click", function () {
   headerList.classList.toggle("active");
   document.querySelector(".body").classList.toggle("lock");
 });
+
+// --- tickets ---//
+
+// let tickets = document.querySelector("tickets");
+
+// tickets.addEventListener("click", () => {
+//   tickets.classList.display = "block";
+// });
